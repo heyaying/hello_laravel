3083f1434bf2493b5c97250f181aea4731ffe7d8
heyaying <648298450@qq.com>
2018-05-03 09:00:30 +0000

    Procfile for Heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   my  Laravel
                 </div>
 
                 <div class="links">
